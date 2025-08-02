c9f42df7d9435bb61a495b495307a9616e1396a6
tanvi kulkarni <tanvi18.kulkarni@gmail.com>
2025-08-02 18:22:38 +0530

    finaldeploypsuh

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,8 @@ app.use(cors(
     {
         origin : "*",
         methods :["GET","POST","PUT","DELETE"],
-        allowedHeaders : ["Content-Type","Authorization"]
+        allowedHeaders : ["Content-Type","Authorization"],
+        credentials: true,
     }
 ))
 app.use(express.json())
