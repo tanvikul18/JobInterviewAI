46f8ae9242e05527ab46d1e3b0db017d700789ec
tanvi kulkarni <tanvi18.kulkarni@gmail.com>
2025-08-02 17:33:59 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv"
 import path from "path"
 import { fileURLToPath } from "url"
 import {dirname} from "path"
-import { connectDb } from "./config/db.js";
-import authRoutes from "./routes/authRoutes.js"
-import sessionRoutes from "./routes/sessionRoutes.js"
-import questionRoutes from "./routes/questionRoutes.js"
-import { protect } from "./middlewares/authMiddleware.js"
-import {generateInterviewQuestion,generateConceptExplanation} from "./controllers/aiController.js"
+import { connectDb } from "./server/config/db.js";
+import authRoutes from "./server/routes/authRoutes.js"
+import sessionRoutes from "./server/routes/sessionRoutes.js"
+import questionRoutes from "./server/routes/questionRoutes.js"
+import { protect } from "./server/middlewares/authMiddleware.js"
+import {generateInterviewQuestion,generateConceptExplanation} from "./server/controllers/aiController.js"
 dotenv.config();
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
